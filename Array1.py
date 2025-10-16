36621c6ba55d0e965c61ee3aaf440a89925f5862
DanialHa <ricardodiaz528491@gmail.com>
2025-10-16 11:43:17 -0400

    Update Array1.py

--- Array1.py
+++ Array1.py
@@ -7,5 +7,5 @@ data= pd.read_csv("Video_Games.csv")
         
 for i in range(len(data)):
     score = data["Critic_Score"][i]   
-    if score > 75:                    
+    if score > 80:                    
         print(data["Name"][i])
