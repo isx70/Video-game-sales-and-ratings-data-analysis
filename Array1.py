fefd70c37d5e4682470fd94b83bbcaa5ceb4d7d3
isx70 <externeisx@outlook.com>
2025-10-16 22:29:28 -0400

    Update Array1.py
    adding the filter to select best games scored above 80.

--- Array1.py
+++ Array1.py
@@ -1,11 +1,17 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 data= pd.read_csv("Video_Games.csv")
 
+best_games = [] 
 
-        
+# This code loop through the data frame and makes a new list where every game
+# that has a score higher than 80 is selected and the rest is filtered out.
 for i in range(len(data)):
     score = data["Critic_Score"][i]   
     if score > 80:                    
-        print(data["Name"][i])
+        best_games.append(data["Name"][i])
+best_games= np.array(best_games)        
+print(best_games)
+        
