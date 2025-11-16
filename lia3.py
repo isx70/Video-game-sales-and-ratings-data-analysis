865fb5ff1306d5067fbd1b998543b053c64610f6
isx70 <externeisx@outlook.com>
2025-11-15 22:31:03 -0500

    Update lia3.py

--- lia3.py
+++ lia3.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plt
 data= pd.read_csv("Video_Games.csv")
 
 #! overview of data
-#print(data.head())
+print(data.head())
 
-#print(data.shape)
+print(data.shape)
 
-#print(data.info())
+print(data.info())
 
-#print(data.describe())
+print(data.describe())
 
 #2 identify and drop duplicates
 
@@ -30,12 +30,12 @@ data['Year_of_Release'] = data['Year_of_Release'].fillna(data['Year_of_Release']
 #data = data.dropna(subset=['User_Score','User_Count',"Critic_Score","Critic_Count","Developer","Rating"])
 
 
-#print(data.isnull().sum())
+print(data.isnull().sum())
 
 #4 Correct data types and formats:
 
 data["User_Score"]=pd.to_numeric(data["User_Score"],errors="coerce")
-#print (data.info())
+print (data.info())
 
 #Part 3 : Univariate non-graphical EDA
 
@@ -50,7 +50,7 @@ for col in numeric:
 summary_num = pd.DataFrame(num_rows, columns=["variable","mean","median","mode",
 "std","var","skew","kurt","q25","q50","q75"])
 
-#print(summary_num)
+print(summary_num)
 
 
 categorical = data.select_dtypes(include='object').columns
@@ -64,7 +64,7 @@ for col in categorical:
 summary_cat = pd.DataFrame(cat_rows, columns=["variable","frequency_counts","proportions",
 "mode","unique_categories"])
 
-#print(summary_cat)
+print(summary_cat)
 
 
 
@@ -156,57 +156,71 @@ print(tab4)
 #6.1
 #a (Q3)
 sns.relplot(data=data, x= "Critic_Score", y= "Global_Sales", col= pd.cut(data["Year_of_Release"], bins=[1995, 2000, 2005, 2010, 2016],labels=["1995–1999", "2000–2004", "2005–2009", "2010–2015"]), kind= "scatter").set(yscale="log")
-#The graph shows that the critic score and global sales have an important correlation
+#The graph shows that the critic score and global sales have an important correlation, that is somewhat the same through the years even tho there is much more games with the years
 
 #b (Q1)
-sns.relplot(data=data[(data["Year_of_Release"] >= 2000) & (data["Year_of_Release"] <= 2016)],x="Critic_Score",y="Global_Sales",hue="User_Score",size="Critic_Count",col="Genre",kind="scatter",col_wrap=3).set(yscale="log")
+sns.relplot(data=data[(data["Year_of_Release"] >= 2000) & (data["Year_of_Release"] <= 2016)],x="User_Score",y="Global_Sales",hue="Critic_Score",size="User_Count",col="Genre",kind="scatter",col_wrap=3).set(yscale="log")
+#The graph shows that critic score, user score and sales are correlated in every genre of video game
 
 #c (Q3)
 sns.relplot(data=data,x="Year_of_Release",y="Critic_Score",kind="line")
+#This graph shows that video game through the year were graded more objectively and more accuretly
 
 #d (Q2)
 data["Score_Diff"] = data["Critic_Score"] / 10 - data["User_Score"]
+#here we added a new column to use it after in the data that shows the difference between the critic score (/10) and the user's
 sns.relplot(data=data,x="Year_of_Release",y="Score_Diff",kind="line",errorbar=("sd"))
+#This graphs shows that user score and critic score became more in lign with each other through the years the journal becoming less bias.
 
 #e (Q1)
-sns.lmplot(data=data[(data["Year_of_Release"] >= 2000) & (data["Year_of_Release"] <= 2016)],x="Critic_Score",y="Global_Sales",line_kws={'color': 'red'}).set(yscale="log")
+sns.lmplot(data=data[(data["Year_of_Release"] >= 2000) & (data["Year_of_Release"] <= 2016)],x="Critic_Score",y="Global_Sales",line_kws={'color': 'red'})
+#This graphs shows very accuretly the relation ship between the critic score and the sales
 
 #_______________________________________________________________________________________________________________________________________
 #6.2
 #a (Q4)
 sns.catplot(data=data[(data["Critic_Score"] >= 80) & (data["Global_Sales"] >= 1)], y="Publisher", x="Critic_Score", kind="strip", jitter=True)
+#this graph shows the compagnies that have the biggest amount of well graded and sold games
 
 #b (Q2)
 sns.catplot(data=data, y="Genre", x="Score_Diff", kind="strip", jitter=False)
+#This graphs shows very well which genre of game have very near or far critic score and user score
 
 #c (Q5)
-sns.catplot(data=data[data["Genre"].isin(data["Genre"].value_counts().index[:6])].sample(150), x="Genre", y="NA_Sales", hue="Rating", kind="swarm").set(yscale="log")
+sns.catplot(data=data[data["Genre"].isin(data["Genre"].value_counts().index[:6])].sample(300), x="Genre", y="NA_Sales", hue="Rating", kind="swarm").set(yscale="log")
 #(limited the sample because this code crashed my spyder)
+#This graphs shows very well which genre is preferred in NA with the rating of each game
 
 #d (Q5)
 sns.catplot(data=data[(data["Genre"].isin(data["Genre"].value_counts().index[:6])) & (data["Rating"].isin(["E", "M"]))],x="Genre", y="EU_Sales", hue="Rating", kind="box",).set(yscale="log")
+#This graphs shows very well which genre is preferred in EU with the rating of each game, it shows also which genre is more mature or for everyone
 
 #e (Q5)
 sns.catplot(data=data[(data["Genre"].isin(data["Genre"].value_counts().index[:6])) & (data["Rating"].isin(["E", "M"]))],x="Genre", y="JP_Sales", hue="Rating", kind="boxen",).set(yscale="log")
+#This graphs shows very well which genre is preferred in JP with the rating of each game, it shows also which genre is more mature or for everyone
 
 #f (Q5)
 sns.catplot(data=data[(data["Genre"].isin(data["Genre"].value_counts().index[:6])) & (data["Rating"].isin(["E", "M"]))],x="Genre", y="NA_Sales", hue="Rating", kind="violin",split=True).set(yscale="log")
 #Here the fatness doesnt show the number but the density, so even if there is more M games in shooter, all E games performed similarly
+#This shows how where the density of games that didnt sell well are in each genre.
 
 #g (Q5)
 g = sns.catplot(data=data[data["Genre"].isin(data["Genre"].value_counts().index[:6])], x="Genre", y="NA_Sales", kind="violin", inner=None).set(yscale="log")
 sns.stripplot(data=data[data["Genre"].isin(data["Genre"].value_counts().index[:6])].sample(1000), x="Genre", y="NA_Sales", color="k", size=2, jitter=True, ax=g.ax)
 #sample because the points were not readable here the dots only appear after 10^-2 because of the nature of the data in which a lot of game have near 0 sales, so only high selling games are showed in the dots
+#This graphs shows which genre is the most popular in NA and which have the higher sales
 
 #h (Q4)
 #sns.catplot(data=data[(data["Critic_Score"] >= 80) & (data["Global_Sales"] >= 1)], y="Publisher", x="Critic_Score", hue="Rating", kind="bar", errorbar=('ci', 97))
 sns.catplot(data=data[(data["Critic_Score"] >= 80) & (data["Global_Sales"] >= 1) & (data["Publisher"].isin(data[(data["Critic_Score"] >= 80) & (data["Global_Sales"] >= 1)]["Publisher"].value_counts().index[:3]))], y="Publisher", x="Critic_Score", hue="Rating", kind="bar", errorbar=('ci', 97))
+#This graph shows the different critic score for each rating for the best 3 publisher of video games.
 
 #i (Q5)
 sns.catplot(data=pd.melt(data[data["Genre"].isin(data["Genre"].value_counts().index[:6])], id_vars="Genre", value_vars=["NA_Sales", "EU_Sales", "JP_Sales"], var_name="Region", value_name="Sales"), x="Genre", y="Sales", hue="Region", kind="point", errorbar=("ci", 90), linestyles="--")
-#A new genre variable was added for this graph to be very precise, even tho var wasn't studied in class.
+#A new "genre" variable was added for this graph to be very precise, even tho var wasn't studied in class.
+#This graphs shows very well which country likes which genre and how much they sell
 
 #j (Q2)
 data["Diff_of_1.5"] = ((data["Critic_Score"] / 10 - data["User_Score"]) >= 1.5)
 sns.catplot(data=data, x="Diff_of_1.5", kind="count")
-
+#This graph shows clearly that the majority of games have similar user score and critic score
